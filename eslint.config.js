204262e7d48e9637a7fb4152c06488448fb8d634
Joel Jolly <128782382+withinJoel@users.noreply.github.com>
2026-04-10 14:14:23 +0530

    Added eslint unused imports package

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,5 @@
+import unusedImports from 'eslint-plugin-unused-imports';
+
 export default [
   {
     ignores: ['dist/**', 'node_modules/**', 'Data/**'],
@@ -8,6 +10,26 @@ export default [
       ecmaVersion: 'latest',
       sourceType: 'module',
     },
-    rules: {},
+    plugins: {
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      // disable default rule (important)
+      'no-unused-vars': 'off',
+
+      // remove unused imports automatically
+      'unused-imports/no-unused-imports': 'error',
+
+      // warn for unused vars/functions
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
   },
 ];
